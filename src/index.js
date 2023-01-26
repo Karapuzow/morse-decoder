bbf2549db21307108b36b9d46f9bc6252f11063f
Karapuzow <118494014+Karapuzow@users.noreply.github.com>
2023-01-27 00:46:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,11 +41,8 @@ const MORSE_TABLE = {
 function decode(expr) {
     let result = ''
     
-    morseCode = expr.replace(/00/g, '')
-        .replace(/10/g, '.')
-        .replace(/11/g, '-')
-        .replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ')
-        .split(' ')
+    morseCode = expr.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ').split(' ')
+       
     for (let num = 0; num < morseCode.length; num++) {
         result = result + MORSE_TABLE[morseCode[num]]
     }
